ae151f55e7b010947e104b68dfecd64ebe5e7245
Mendjy23 <100388745+Mendjy23@users.noreply.github.com>
2022-10-04 00:32:26 +0000

    main.py

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -2,21 +2,21 @@ def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
   i = 0
    
   if len(figurinhas_da_maria) < len(figurinhas_do_joao):
-        for i in range(len(figurinhas_da_maria)):
-            for z in range(len(figurinhas_do_joao)):
-                if figurinhas_da_maria[i] == figurinhas_do_joao[z]:
-                    i += 1
+    for i in range(len(figurinhas_da_maria)):
+      for z in range(len(figurinhas_do_joao)):
+      if figurinhas_da_maria[i] == figurinhas_do_joao[z]:
+        i += 1
         quantidade = len(figurinhas_da_maria) - i
 
-    elif len(figurinhas_do_joao) < len(figurinhas_da_maria):
-        for i in range(len(figurinhas_do_joao)):
-            for z in range(len(figurinhas_da_maria)):
-                if figurinhas_do_joao[i] == figurinhas_da_maria[z]:
-                    i += 1
-        quantidade = len(figurinhas_do_joao) - i
+  elif len(figurinhas_do_joao) < len(figurinhas_da_maria):
+      for i in range(len(figurinhas_do_joao)):
+        for z in range(len(figurinhas_da_maria)):
+          if figurinhas_do_joao[i] == figurinhas_da_maria[z]:
+            i += 1
+      quantidade = len(figurinhas_do_joao) - i
 
 
-    return quantidade
+  return quantidade
 
 
 
