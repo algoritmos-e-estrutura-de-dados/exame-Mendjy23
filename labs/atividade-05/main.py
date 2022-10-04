97bfa7945c2b8a95be3333d5ad724454a43cb96a
Mendjy23 <100388745+Mendjy23@users.noreply.github.com>
2022-10-04 01:12:17 +0000

    main.py

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,17 +1,21 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-  c = None
-  while c != 0:
-    figurinhas_da_maria = figurinhas_do_joao
-    figurinhas_do_joao = c
-    return figurinhas_da_maria 
-
-  trocas = int(input())
-  for i in range(trocas):
-    numeros = [int(num) for num in input().strip().split(' ')]
-    print(maximizar_troca_de_figurinhas(numeros[0], numeros[1]))  
-    
+  i = 0
    
-  
+  if len(figurinhas_da_maria) < len(figurinhas_do_joao):
+    for i in range(len(figurinhas_da_maria)):
+      for z in range(len(figurinhas_do_joao)):
+        if figurinhas_da_maria[i] == figurinhas_do_joao[z]:
+          i += 1
+          name = len(figurinhas_da_maria) - i
+
+  elif len(figurinhas_do_joao) < len(figurinhas_da_maria):
+      for i in range(len(figurinhas_do_joao)):
+        for z in range(len(figurinhas_da_maria)):
+          if figurinhas_do_joao[i] == figurinhas_da_maria[z]:
+            i += 1
+      name = len(figurinhas_do_joao) - i
+
+      return name
 
 
 
@@ -21,5 +25,4 @@ if __name__ == '__main__':
   A, B = input().split(' ')
   figurinhas_da_maria = input().split(' ')
   figurinhas_da_joao = input().split(' ')
-  maximizar_troca_de_figurinhas(figurinhas_da_maria, 
- figurinhas_da_joao)
+  maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
