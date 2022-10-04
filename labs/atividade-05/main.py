57bc1c5b55b9121d2af06c566e88a9148e520f87
Mendjy23 <100388745+Mendjy23@users.noreply.github.com>
2022-10-04 00:52:31 +0000

    main.py

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -22,7 +22,7 @@ def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
 
 
 if __name__ == '__main__':
-    A, B = input().split(' ')
-    figurinhas_da_maria = input().split(' ')
-    figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
+  A, B = input().split(' ')
+  figurinhas_da_maria = input().split(' ')
+  figurinhas_da_joao = input().split(' ')
+  maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
